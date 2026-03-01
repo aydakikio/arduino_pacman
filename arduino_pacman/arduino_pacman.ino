3a80571cb2e73606af2ada77b005598ea9557310
aydakikio <kikioayda24@gmail.com>
2026-03-01 20:14:21 +0330

    Version 0.1

--- arduino_pacman/arduino_pacman.ino
+++ arduino_pacman/arduino_pacman.ino
@@ -1,7 +1,7 @@
 #include <U8g2lib.h>
 
 // ===== HARDWARE CONFIGURATION =====
-U8G2_SH1106_128X64_NONAME_2_HW_I2C u8g2(U8G2_R0, -1, A5, A4);
+U8G2_SH1106_128X64_NONAME_2_HW_I2C u8g2(U8G2_R3, -1, A5, A4);
 
 // Button pin definitions
 #define BTN_RIGHT 10
@@ -35,45 +35,62 @@ void loop() {
 
 void draw_borders(){
 
+  u8g2.setFont(u8g2_font_4x6_tf);
+  u8g2.drawStr(1, 6, "Score:\ ");
+
+
+  u8g2.drawHLine(0, 10, 63);
+
+  u8g2.drawVLine(0, 10, 17);
+  u8g2.drawVLine(63, 10, 15);
+
+  u8g2.drawVLine(0, 42, 10);
+  u8g2.drawVLine(63, 42, 10);
+
+  u8g2.drawHLine(0, 51, 5);
+  u8g2.drawHLine(59, 51, 5);
+
+  u8g2.drawVLine(4, 51, 10);
+  u8g2.drawVLine(58, 51, 10);
+
+
+  u8g2.drawHLine(0, 61, 5);
+  u8g2.drawHLine(58, 61, 5);
+
+
+
+  u8g2.drawHLine(0, 76, 5);
+  u8g2.drawHLine(58, 76, 5);
+
+
+
+  u8g2.drawVLine(4, 76, 10);
+  u8g2.drawVLine(58, 76, 10);
+
+
+  u8g2.drawHLine(0, 86, 5);
+  u8g2.drawHLine(58, 86, 5);
+
+
+  u8g2.drawVLine(0, 86, 12);
+  u8g2.drawVLine(63, 86, 12);
+
+
+  u8g2.drawVLine(0, 113, 15);
+  u8g2.drawVLine(63, 113, 15);
+
+
+  u8g2.drawHLine(0, 127, 64);
 }
+
 void draw_game(){
   u8g2.firstPage();
   do {
-    u8g2.drawVLine(0, 0, 63);
     
-    u8g2.drawHLine(0, 0, 15);
-    u8g2.drawHLine(0, 63, 15);
+    draw_borders();
 
-    u8g2.drawHLine(30, 0, 10);
-    u8g2.drawHLine(30, 63, 10);
+    //u8g2.drawHLine(0,10,128);
+    //u8g2.drawFrame(0, 0, 40, 15);
 
-    u8g2.drawVLine(40, 0, 8);
-    u8g2.drawVLine(40, 53, 10);
-
-    u8g2.drawHLine(40, 10, 15);
-    u8g2.drawHLine(40, 53, 15);
-
-    u8g2.drawVLine(54, 0, 10);
-    u8g2.drawVLine(54, 53, 10);
-
-    u8g2.drawVLine(69, 0, 10);
-    u8g2.drawVLine(69, 53, 10);
-
-    u8g2.drawHLine(69, 10, 15);
-    u8g2.drawHLine(69, 53, 15);
-
-    u8g2.drawVLine(84, 0, 10);
-    u8g2.drawVLine(84, 53, 10);
-
-    u8g2.drawHLine(84, 0, 10);
-    u8g2.drawHLine(84, 63, 10);
-
-    u8g2.drawHLine(109, 0, 10);
-    u8g2.drawHLine(109, 63, 10);
-
-    u8g2.drawVLine(118, 0, 64);
-
-
-    //u8g2.drawFrame(0, 0, 40, 20);
   } while (u8g2.nextPage());
 }
